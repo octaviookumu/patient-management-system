8fbc803cebd6168f76a7e7e5d5b697b7c9c0a871
octaviookumu <octaviookumu@gmail.com>
2026-09-22 01:13:40 +0300

    feat: add patient repo, service and impl

--- patient-service/src/main/java/com/octaviookumu/patient_service/services/PatientService.java
+++ patient-service/src/main/java/com/octaviookumu/patient_service/services/PatientService.java
@@ -1,9 +1,12 @@
 package com.octaviookumu.patient_service.services;
 
+import com.octaviookumu.patient_service.domain.CreatePatientRequest;
 import com.octaviookumu.patient_service.domain.entities.Patient;
 
 import java.util.List;
 
 public interface PatientService {
     List<Patient> getPatients();
+
+    Patient createPatient(CreatePatientRequest createPatientRequest);
 }
--- patient-service/src/main/java/com/octaviookumu/patient_service/services/impl/PatientServiceImpl.java
+++ patient-service/src/main/java/com/octaviookumu/patient_service/services/impl/PatientServiceImpl.java
@@ -1,10 +1,13 @@
 package com.octaviookumu.patient_service.services.impl;
 
+import com.octaviookumu.patient_service.domain.CreatePatientRequest;
 import com.octaviookumu.patient_service.domain.entities.Patient;
+import com.octaviookumu.patient_service.mappers.PatientMapper;
 import com.octaviookumu.patient_service.repositories.PatientRepository;
 import com.octaviookumu.patient_service.services.PatientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,5 +20,18 @@ public class PatientServiceImpl implements PatientService {
     @Override
     public List<Patient> getPatients() {
         return patientRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public Patient createPatient(CreatePatientRequest createPatientRequest) {
+        String email = createPatientRequest.getEmail();
+        patientRepository.findByEmail(email).ifPresent(existing -> {
+            throw new IllegalStateException("Patient with email " + email + " already exists");
+        });
+
+        Patient newPatient = PatientMapper.toPatient(createPatientRequest);
+
+        return patientRepository.save(newPatient);
     }
 }
