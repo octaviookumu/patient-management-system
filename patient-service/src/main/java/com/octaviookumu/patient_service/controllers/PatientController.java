bdd0e525a4906e0f3fd8956ce9588f43321f3629
octaviookumu <octaviookumu@gmail.com>
2026-09-21 23:11:09 +0300

    feat: add patient controller layer

---
+++ patient-service/src/main/java/com/octaviookumu/patient_service/controllers/PatientController.java
@@ -0,0 +1,29 @@
+package com.octaviookumu.patient_service.controllers;
+
+import com.octaviookumu.patient_service.domain.dtos.PatientResponseDto;
+import com.octaviookumu.patient_service.mappers.PatientMapper;
+import com.octaviookumu.patient_service.services.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/patients")
+@RequiredArgsConstructor
+public class PatientController {
+
+    private final PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDto>> getPatients() {
+        List<PatientResponseDto> patientResponseDtos = patientService.getPatients().stream()
+                .map(PatientMapper::toDto).toList();
+        return ResponseEntity.ok(patientResponseDtos);
+    }
+
+
+}
