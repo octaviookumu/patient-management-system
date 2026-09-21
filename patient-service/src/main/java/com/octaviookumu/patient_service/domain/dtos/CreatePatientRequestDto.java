023e06ab678a69c69f570f7d2d93331cadf587a9
octaviookumu <octaviookumu@gmail.com>
2026-09-22 01:12:22 +0300

    refactor: PatientRequestDto to CreatePatientRequestDto

---
+++ patient-service/src/main/java/com/octaviookumu/patient_service/domain/dtos/CreatePatientRequestDto.java
@@ -0,0 +1,32 @@
+package com.octaviookumu.patient_service.domain.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class CreatePatientRequestDto {
+    @NotBlank
+    @Size(max = 100, message = "Name cannot exceed {max} characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotBlank(message = "Registered date is required")
+    private String registeredDate;
+
+}
