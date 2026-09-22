1378b14345c4e821c24beb3fdd947a16c32c3ed9
octaviookumu <octaviookumu@gmail.com>
2026-09-22 13:21:10 +0300

    feat: add request validation handler (exception handler) - hides implementation details from clients that are spring boot specific

---
+++ patient-service/src/main/java/com/octaviookumu/patient_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,71 @@
+package com.octaviookumu.patient_service.exceptions;
+
+import com.octaviookumu.patient_service.domain.dtos.ApiErrorResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * Handler for the base exception class
+     * <p>
+     * If we are catching a base exception, that means something has gone quite wrong
+     * </p>
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponseDto> handleException(Exception ex) {
+        log.error("Caught exception", ex);
+        ApiErrorResponseDto error = ApiErrorResponseDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Catches validation errors (such as Email address not valid, Email address required)
+     *
+     * @param ex MethodArgumentNotValidException
+     * @return ApiErrorResponseDto
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponseDto> handleException(MethodArgumentNotValidException ex) {
+
+
+        // Transform Spring's FieldErrors into your custom ApiErrorResponseDto.FieldError list
+        List<ApiErrorResponseDto.FieldError> validationErrors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(error -> ApiErrorResponseDto.FieldError.builder()
+                        .field(error.getField())
+                        .message(error.getDefaultMessage())
+                        .build())
+                .toList();
+
+        // Build the unified DTO response
+        ApiErrorResponseDto errorPayload = ApiErrorResponseDto.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("Validation failed for one or more fields.")
+                .errors(validationErrors)
+                .build();
+
+        return ResponseEntity.badRequest().body(errorPayload);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ApiErrorResponseDto> handleIllegalStateException(IllegalStateException ex) {
+        ApiErrorResponseDto error = ApiErrorResponseDto.builder()
+                .status(HttpStatus.CONFLICT.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+}
